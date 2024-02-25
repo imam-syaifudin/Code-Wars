729a480ce090f7093c467e346c2705f19549d6aa
Muhammad Imam <saitamasensei2005@gmail.com>
2024-02-25 20:01:29 +0700

    How many pages in a book

--- How many pages in a book.js
+++ How many pages in a book.js
@@ -1,16 +1,17 @@
 function amountOfPages(summary){
 
     let list = "";
-    let i = 0;
-    while( i <= 17 ){
+    let i = 1;
+    let lastNumber = 0;
+    while( list.length !== summary ){
         list += `${i}`;
+        lastNumber = i;
         i++
-        list.length;
     }
 
-    return list;
+    return lastNumber;
 
 }
 
-// console.log(amountOfPages(5))
+console.log(amountOfPages(5))
 console.log(amountOfPages(25))
