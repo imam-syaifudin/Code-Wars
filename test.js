8fd0f0e20d17c9efa9cf190d5309280ede5294f5
Muhammad Imam <saitamasensei2005@gmail.com>
2024-04-03 11:15:12 +0700

    TEST JS

--- test.js
+++ test.js
@@ -1,16 +1,31 @@
-const country =  [
-    { "name": "Afghanistan", "code": "AF" },
-    { "name": "Åland Islands", "code": "AX" },
-    { "name": "Albania", "code": "AL" },
-    { "name": "Algeria", "code": "DZ" },
+const data = [
+  [0, 1, 2, 3, 45],
+  [10, 11, 12, 13, 14],
+  [20, 21, 22, 23, 24],
+  [30, 31, 32, 33, 34],
 ];
 
-const mapping = country.map(val => { 
-    return {
-        name: val.name,
-        code: val.code,
-        value: `${val.code} - ${val.name}`
-    }
-})
+function toCsvText(array) {
+  let result = "";
 
-console.log(mapping);
+  for (let i = 0; i < array.length; i++) {
+    if (i + 1 !== array.length) array[i].push("\n");
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if ( array[i][j] !== "\n" && array[i][j + 1] !== "\n" ) result += `${array[i][j]},`;
+      else if( array[i][j + 1] === "\n" ) result += `${array[i][j]}`
+      else result += "\\n"
+    }
+  }
+
+  result = result.split("");
+  result.pop();
+
+  return result.join("");
+}
+
+console.log(toCsvText(data));
+console.log('0,1,2,3,45\\n10,11,12,13,14\\n20,21,22,23,24\\n30,31,32,33,34')
+
