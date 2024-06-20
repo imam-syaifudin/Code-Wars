66cb823cd55372475fc99c51b5e5f7927ab012c2
Muhammad Imam <saitamasensei2005@gmail.com>
2024-06-20 17:49:24 +0700

    First Work

--- blowingBIrthday.js
+++ blowingBIrthday.js
@@ -1,46 +1,99 @@
 function blowCandles(str) {
-  let move = 0;
+
   let split = str.split("");
+  let move = 0;
 
-  while (split.some((val) => val !== "0")) {
+  const getNumber = (start = false) => {
 
-    let getThreeFirst = [];
-    let getThreeIndex = [];
-
+    let result = [];
+    let index  = [];
     let i = 0;
 
-    while (getThreeFirst.length !== 3) {
-        
-      const next = split[i + 1];
-      const prev = split[i - 1];
-      const curr = split[i];
-
-      if (curr === "0") {
-        i++;
-        continue;
-      } else {
-        getThreeFirst.push(curr);
-        getThreeIndex.push(i);
+    if( !start ){
+      let setIndex = 0;
+      while( split[setIndex] === "0" ){
+        setIndex++;
+        i = setIndex;
       }
-      i++;
+    } else {
+      i = split.filter(val => val === "0").length
     }
 
-    getThreeIndex.forEach((val, index) => {
-      if ( typeof getThreeFirst[index] !== "undefined" && getThreeFirst[index] !== "0" ) {
-        split[val] = parseInt(getThreeFirst[index]) - 1;
-        split[val] = split[val].toString();
+    while (result.length !== 3) {
+        result.push(split[i]);
+        index.push(i);
+        i++;
+    }
+
+    return { 
+      number: result.filter((val) => typeof val !== "undefined"),
+      indexList:  index,
+    }
+    
+  };
+
+  while (split.some((val) => val !== "0")) {
+    
+    let { number, indexList } = getNumber();
+    let i = 0;
+
+    const doLoop = () => {
+
+      while (number.filter((val) => val === "0").length !== number.length) {
+
+        
+        console.log('group number before',number)
+        
+        if( typeof number[i + 1] !== 'undefined' && number[i + 1] !== "0") { 
+          number[i + 1] = parseInt(number[i + 1]) - 1;
+          number[i + 1] = number[i + 1].toString();
+          split[indexList[i + 1]] = number[i + 1]; 
+        }
+        
+        if( typeof number[i + 2] !== 'undefined' && number[i + 2] !== "0") { 
+          number[i + 2] = parseInt(number[i + 2]) - 1;
+          number[i + 2] = number[i + 2].toString();
+          split[indexList[i + 2]] = number[i + 2]; 
+        }
+        
+        if ( number[i] !== "0") {
+          number[i] = parseInt(number[i]) - 1;
+          number[i] = number[i].toString();
+          split[indexList[i]] = number[i];
+        }
+        
+        if( number[i] === "0" ){
+          split[indexList[i]] = number[i]; 
+          number = getNumber().number;
+          indexList = getNumber().indexList;
+        }
+
+        if( number.every(val => val === "0") ){
+          number = getNumber(true).number;
+          indexList = getNumber(true).indexList
+        }
+
+        console.log('split after', split)
+        console.log('\n')
+        move++;
+
       }
-    });
 
-    console.log(getThreeFirst)
+    }
 
-    getThreeFirst = [];
-    getThreeIndex = [];
-    // move++;
+    doLoop();
   }
+
 
   return move;
 }
 
-console.log(blowCandles("1321"));
-console.log(blowCandles("2113"));
+// Basic Test
+// console.log(blowCandles("1321")) // 3],
+// console.log(blowCandles("0323456")) // 9],
+// console.log(blowCandles("2113")) // 5],
+// console.log(blowCandles("1110")) // 1],
+// console.log(blowCandles("121")) // 2]
+
+// Fail Test
+console.log(blowCandles("102201")) // 2]
