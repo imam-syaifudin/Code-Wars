cf7b2cbb4de0f69a937d6bee0c83566df1a1cd0d
Muhammad Imam <saitamasensei2005@gmail.com>
2024-06-20 18:25:17 +0700

    First Work

--- blowingBIrthday.js
+++ blowingBIrthday.js
@@ -1,99 +1,62 @@
-function blowCandles(str) {
-
+const blowCandles = (str) => {
+  
   let split = str.split("");
   let move = 0;
 
-  const getNumber = (start = false) => {
-
-    let result = [];
-    let index  = [];
+  const getNumber = () => {
+    let result    = [];
+    let indexList = [];
     let i = 0;
 
-    if( !start ){
-      let setIndex = 0;
-      while( split[setIndex] === "0" ){
-        setIndex++;
-        i = setIndex;
-      }
-    } else {
-      i = split.filter(val => val === "0").length
-    }
+    while (split[i] === "0") i++;
 
     while (result.length !== 3) {
-        result.push(split[i]);
-        index.push(i);
-        i++;
+      result.push(split[i]);
+      indexList.push(i);
+      i++;
     }
 
-    return { 
-      number: result.filter((val) => typeof val !== "undefined"),
-      indexList:  index,
-    }
-    
+    return { number: result.filter((val) => typeof val !== "undefined"), indexList: indexList };
   };
 
   while (split.some((val) => val !== "0")) {
-    
+
     let { number, indexList } = getNumber();
     let i = 0;
 
-    const doLoop = () => {
+    const changeSplit = (index) => {
+      number[index] = parseInt(number[index]) - 1;
+      number[index] = number[index].toString();
+      split[indexList[index]] = number[index];
+    };
 
-      while (number.filter((val) => val === "0").length !== number.length) {
+    while (number.some((val) => val !== "0")) {
+      
+      const nextNumber = number[i + 1];
+      const next2Number = number[i + 2];
 
-        
-        console.log('group number before',number)
-        
-        if( typeof number[i + 1] !== 'undefined' && number[i + 1] !== "0") { 
-          number[i + 1] = parseInt(number[i + 1]) - 1;
-          number[i + 1] = number[i + 1].toString();
-          split[indexList[i + 1]] = number[i + 1]; 
-        }
-        
-        if( typeof number[i + 2] !== 'undefined' && number[i + 2] !== "0") { 
-          number[i + 2] = parseInt(number[i + 2]) - 1;
-          number[i + 2] = number[i + 2].toString();
-          split[indexList[i + 2]] = number[i + 2]; 
-        }
-        
-        if ( number[i] !== "0") {
-          number[i] = parseInt(number[i]) - 1;
-          number[i] = number[i].toString();
-          split[indexList[i]] = number[i];
-        }
-        
-        if( number[i] === "0" ){
-          split[indexList[i]] = number[i]; 
-          number = getNumber().number;
-          indexList = getNumber().indexList;
-        }
+      if (typeof nextNumber  !== "undefined" && nextNumber  !== "0") changeSplit(i + 1);
+      if (typeof next2Number !== "undefined" && next2Number !== "0") changeSplit(i + 2);
+      if (number[i] !== "0") changeSplit(i);
 
-        if( number.every(val => val === "0") ){
-          number = getNumber(true).number;
-          indexList = getNumber(true).indexList
-        }
-
-        console.log('split after', split)
-        console.log('\n')
-        move++;
-
+      if (number[i] === "0" || number.every((val) => val === "0")) {
+        number    = getNumber().number;
+        indexList = getNumber().indexList;
       }
 
+      move++;
     }
-
-    doLoop();
   }
 
-
   return move;
-}
+};
 
 // Basic Test
-// console.log(blowCandles("1321")) // 3],
-// console.log(blowCandles("0323456")) // 9],
-// console.log(blowCandles("2113")) // 5],
-// console.log(blowCandles("1110")) // 1],
-// console.log(blowCandles("121")) // 2]
+console.log(blowCandles("1321")) // 3],
+console.log(blowCandles("0323456")); // 9],
+console.log(blowCandles("2113")) // 5],
+console.log(blowCandles("1110")) // 1],
+console.log(blowCandles("121")) // 2]
 
 // Fail Test
-console.log(blowCandles("102201")) // 2]
+console.log(blowCandles("102201")); // 2
